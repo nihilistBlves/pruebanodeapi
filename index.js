927cb80a0a27f147bf84750415cea445c3625635
Alejo Perez Benayas <aperez@amurait.com>
2022-06-30 16:43:44 +0200

    Node.js Api

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const router = require("./router");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+const PORT = 8000;
+
+dotenv.config();
+
+const app = express();
+
+app.listen(PORT, async() => {
+    console.log(`server up on port ${PORT}`);
+});
+
+mongoose.connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    console.log("Connected to MongoDB");
+}).catch((err) => {
+    console.log(err);
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(router);
+
